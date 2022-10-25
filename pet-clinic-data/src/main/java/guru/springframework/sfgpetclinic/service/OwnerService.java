34f207f0c23f295a789f4432582bab91af1754e3
1yashsharma1 <syash832@gmail.com>
2022-10-25 19:29:19 +0530

    refractoring to common service interface. closes #3

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/OwnerService.java
@@ -1,15 +1,8 @@
 package guru.springframework.sfgpetclinic.service;
 
-import java.util.Set;
-
 import guru.springframework.sfgpetclinic.model.Owner;
 
-public interface OwnerService {
-    Owner findbyId(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
